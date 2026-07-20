eb996e24c120718806eb395f02a553ad2ff7eb96
haneulzcreator <haneulzcreator@gmail.com>
2026-07-20 12:19:10 -0400

    Update LoginInput model fields to use correct types

--- backend/server.py
+++ backend/server.py
@@ -70,8 +70,8 @@ async def get_current_admin(request: Request) -> dict:
 
 
 class LoginInput(BaseModel):
-    email: haneulzcreator_db_user
-    password: F1xLJXNS4e64EWHG
+    email: EmailStr
+    password: str
 
 
 class AUCreate(BaseModel):
