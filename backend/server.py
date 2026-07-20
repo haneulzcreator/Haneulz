30b2040f1299973ef878842d5c4f64f4f22081cc
haneulzcreator <haneulzcreator@gmail.com>
2026-07-19 20:08:38 -0400

    Update LoginInput with hardcoded credentials
    Replaced email and password in LoginInput with specific values.

--- backend/server.py
+++ backend/server.py
@@ -70,8 +70,8 @@ async def get_current_admin(request: Request) -> dict:
 
 
 class LoginInput(BaseModel):
-    email: EmailStr
-    password: str
+    email: HANEULZCREATOR@gmail.com
+    password: Princess070508!
 
 
 class AUCreate(BaseModel):
