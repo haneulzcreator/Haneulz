66dd4d280bb7549be39a5b27619d1598952de913
haneulzcreator <haneulzcreator@gmail.com>
2026-08-07 12:17:12 -0400

    Replace icons with images in memoryData.js

--- frontend/src/Game/memoryData.js
+++ frontend/src/Game/memoryData.js
@@ -2,42 +2,17 @@ const cards = [
   {
     id: 1,
     title: "JL",
-    icon: "🦌",
+    image: "/images/jl.jpg",
   },
   {
     id: 2,
     title: "Han",
-    icon: "🐈",
+    image: "/images/han.jpg",
   },
   {
     id: 3,
     title: "HANEULZ",
-    icon: "☁️",
-  },
-  {
-    id: 4,
-    title: "AHOF",
-    icon: "✨",
-  },
-  {
-    id: 5,
-    title: "Universe League",
-    icon: "🏆",
-  },
-  {
-    id: 6,
-    title: "The Little Prince",
-    icon: "🎤",
-  },
-  {
-    id: 7,
-    title: "FOHA",
-    icon: "💙",
-  },
-  {
-    id: 8,
-    title: "HANSUM",
-    icon: "💗",
+    image: "/images/haneulz.jpg",
   },
 ];
 
