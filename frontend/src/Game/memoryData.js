145ebe2287108e68bb23175765b8907d8c6084b4
haneulzcreator <haneulzcreator@gmail.com>
2026-08-12 19:18:08 -0400

    Update memoryData.js

--- frontend/src/Game/memoryData.js
+++ frontend/src/Game/memoryData.js
@@ -1,26 +1,69 @@
-const cards = [
+const memoryData = [
   {
-    id: 1,
-    title: "JL",
-    image: "/images/jl.jpg",
+    uuid: "han-1-a",
+    title: "han-1",
+    image: "/memory/han-1.jpg",
   },
   {
-    id: 2,
-    title: "Han",
-    image: "/images/han.jpg",
+    uuid: "han-1-b",
+    title: "han-1",
+    image: "/memory/han-1.jpg",
+  },
+
+  {
+    uuid: "han-2-a",
+    title: "han-2",
+    image: "/memory/han-2.jpg",
   },
   {
-    id: 3,
-    title: "HANEULZ",
-    image: "/images/haneulz.jpg",
+    uuid: "han-2-b",
+    title: "han-2",
+    image: "/memory/han-2.jpg",
+  },
+
+  {
+    uuid: "han-3-a",
+    title: "han-3",
+    image: "/memory/han-3.jpg",
+  },
+  {
+    uuid: "han-3-b",
+    title: "han-3",
+    image: "/memory/han-3.jpg",
+  },
+
+  {
+    uuid: "jl-1-a",
+    title: "jl-1",
+    image: "/memory/jl-1.jpg",
+  },
+  {
+    uuid: "jl-1-b",
+    title: "jl-1",
+    image: "/memory/jl-1.jpg",
+  },
+
+  {
+    uuid: "jl-2-a",
+    title: "jl-2",
+    image: "/memory/jl-2.jpg",
+  },
+  {
+    uuid: "jl-2-b",
+    title: "jl-2",
+    image: "/memory/jl-2.jpg",
+  },
+
+  {
+    uuid: "jl-3-a",
+    title: "jl-3",
+    image: "/memory/jl-3.jpg",
+  },
+  {
+    uuid: "jl-3-b",
+    title: "jl-3",
+    image: "/memory/jl-3.jpg",
   },
 ];
 
-export default [...cards, ...cards]
-  .sort(() => Math.random() - 0.5)
-  .map((card, index) => ({
-    ...card,
-    uuid: index,
-    flipped: false,
-    matched: false,
-  }));
+export default memoryData;
